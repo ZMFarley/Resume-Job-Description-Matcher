ce93f3e7dbf48757165ffccb62ab46a547b23eb0
ZMFarley <zacharymfarley@gmail.com>
2026-04-02 14:41:36 -0400

    Added metrics file for eventual output

--- backend/metrics.py
+++ backend/metrics.py
@@ -1,3 +1,10 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, confusion_matrix, roc_auc_score, roc_curve, RocCurveDisplay
+import matplotlib.pyplot as plt
+import numpy as np
+import joblib
+
+#This file is dir
 #Calculate relevant metrics and graphs for classifier performance
 def __output_metrics(clf: LogisticRegression, training_embedding_matrix: np.ndarray, label_matrix: np.ndarray, predicted_results: np.ndarray) -> None:
     #Calculate relevant metrics and graph for the classifier
