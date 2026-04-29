b09064cce95a4699909003689f1c882437962837
ZMFarley <zacharymfarley@gmail.com>
2026-04-29 12:05:57 -0400

    made final project specific section for quick iteration

--- backend/metrics.py
+++ backend/metrics.py
@@ -13,6 +13,6 @@ def __output_metrics(clf: LogisticRegression, training_embedding_matrix: np.ndar
     print("Reciever Operating Characteristic Area Under the Curve Score", roc_auc_score(label_matrix, clf.predict_proba(training_embedding_matrix)[:, 1]))
     plot = RocCurveDisplay.from_predictions(label_matrix, clf.predict_proba(training_embedding_matrix)[:, 1], plot_chance_level = True)
     _ = plot.ax_.set(xlabel="False Positive Rate", ylabel="True Positive Rate",
-    title="Trainning AI vs Real Image Detection\nReceiver Operating Characteristic",
+    title="Training AI vs Real Image Detection\nReceiver Operating Characteristic",
     )
     plt.show()
